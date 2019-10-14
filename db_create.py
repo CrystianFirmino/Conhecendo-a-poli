293545e75f032f0924e7eb25d909a5df969a88e8
Marina Daumas Carneiro <marina.daumas@poli.ufrj.br>
2019-10-14 15:00:05 -0300

    atualização db_create (adcionarEvento)

--- db_create.py
+++ db_create.py
@@ -1,7 +1,5 @@
 import sqlite3
 
-
-
 class Banco():
     def criarTabelas(self):
         connection = sqlite3.connect('db1.db')
@@ -14,21 +12,22 @@ class Banco():
                 usuario TEXT NOT NULL,
                 email TEXT NOT NULL,
                 senha TEXT NOT NULL
-                )
+                );
         """
         )
 
         cursor.execute (
         """
-            CREATE TABLE IF NOT EXISTS eventos (
+            CREATE TABLE eventos (
                 id INTEGER PRIMARY KEY,
                 nome TEXT NOT NULL, 
                 descricao TEXT NOT NULL,
                 local TEXT,
-                horario de inicio TEXT,
-                horario de acabar TEXT,
+                data TEXT,
+                horario_de_inicio TEXT NOT NULL,
+                horario_de_fim TEXT NOT NULL
                 
-            )
+            );
         """
         )
 
@@ -41,24 +40,40 @@ class Banco():
                 sala TEXT,
                 descricao TEXT NOT NULL
 
-            )
+            );
         """
         )
         
         connection.commit()
+        cursor.close()
 
-    def adicionarEvento():
+    def adicionarEvento(self, nome, descricao, local, data, horarioIn, horarioFim):
 
-    def cadastrarPessoa():
-    
-    def logarPessoa():
+        cnn = sqlite3.connect('db1.db')
+        cursor = cnn.cursor()    
+     
+        ex = (
+        """
+            INSERT INTO eventos(nome, descricao, local, data, horario_de_inicio, horario_de_fim) 
+                VALUES (?, ?, ?, ?, ?, ?);       
+        """
+        )
+        cursor.execute((ex), (nome, descricao, local, data, horarioIn, horarioFim))
+        cnn.commit()
+        cursor.close()
 
-    def aceitarEvento():
-
-    def listarEventos():
-    
+    def cadastrarPessoa(self):
+        x = 1
+    def logarPessoa(self):
+        x = 1
+    def aceitarEvento(self):
+        x = 1
+    def listarEventos(self):
+        x = 1
 
 
 
 banco = Banco()
-banco.criarTabelas()
+
+
+
