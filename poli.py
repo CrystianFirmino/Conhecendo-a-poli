285448a7fa4d4b865dc975ad27d5d4aae6a9125d
Marina Daumas Carneiro <marina.daumas@poli.ufrj.br>
2019-10-14 21:30:02 -0300

    separei classes de pessoas do arquivo principal poli

--- poli.py
+++ poli.py
@@ -1,72 +1,9 @@
 from flask import Flask, request, render_template, redirect, url_for, session
 import os
 from db_create import Banco
+from pessoas import Pessoa, Usuario, Coordenador, Adm
 
 app = Flask(__name__)
-
-class Pessoa:
-    def __init__(self):
-        self.__classe = "none"
-        self.iniciar( 0, 0, 0)
-
-    def iniciar(self, nome, senha, email):
-        self.priority = 0
-        self._nome = nome
-        self._email = email
-        self._senha=senha
-        self.valido = self.validar()
-    
-    def validar(self):
-        self.set_priority()
-        #Para (futuramente) verificar se a pessoa é valida
-        valido = True
-        return valido
-
-    def get_classe(self):
-        return self.__classe
-    
-    def set_priority(self):
-        self.priority = 0
-        if self.get_classe() == "usuario":
-            self.priority = 2
-        if self.get_classe() == "coordenador":
-            self.priority = 4
-        if self.get_classe() == "adm":
-            self.priority = 5
-
-
-
-    def get_nome(self):
-        return self._nome
-
-    def alterar_senha(self):
-        return "Ainda nao e possivel alterar a senha"
-        
-class Usuario(Pessoa):
-    def __init__(self, nome, senha, email):
-        self.__classe = "usuario"
-        self.iniciar(nome, senha, email)
- 
-    def get_classe(self):
-        return self.__classe
-
-class Coordenador(Pessoa):
-    def __init__(self, nome, senha, email):
-        self.__classe = "coordenador"
-        self.iniciar(nome, senha, email)
- 
-    def get_classe(self):
-        return self.__classe
-
-class Adm(Coordenador):
-    def __init__(self, nome, senha, email):
-        self.__classe = "adm"
-        self.iniciar(nome, senha, email)
- 
-    def get_classe(self):
-        return self.__classe
-
-
 
 class bBanco:
     def __init__(self):
