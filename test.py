630235e3724e4c74f7fe67989024ff0c49fae28f
Marina Daumas Carneiro <marina.daumas@poli.ufrj.br>
2019-12-11 10:12:31 -0300

    add assuntos table

--- test.py
+++ test.py
@@ -11,18 +11,6 @@ result = []
 user = 3
 with sqlite3.connect('db1.db') as connection:
     cursor = connection.cursor()
-    # # cursor.execute("""INSERT INTO grade (userId, segunda, terca, quarta, quinta, sexta, sabado) 
-    # #                             VALUES (?, '', '', '', '', '', '')""", (user,))
-    
-    # tbl = cursor.execute("SELECT segunda, terca, quarta, quinta, sexta, sabado FROM grade WHERE userId = ?", (user,)).fetchall()
-    # print(tbl)
-    # connection.commit()
 
-    semat = [('rber, rveertv, , vert, erver, ,', ' , cwec, wcew, , , , ', 'rber, rveertv, , vert, erver, ,', ' , cwec, wcew, , , , ''rber, rveertv, , vert, erver, ,', ' , cwec, wcew, , , , ')]
-
-    semat = list(semat[0])
-
-    for i in range(len(semat)):
-        semat[i] = semat[i].split(",")
-    print(semat)
-        
+    cursor.execute("INSERT INTO assuntos (nome) VALUES ('Ambiental'),('Controle e Automação'),('Materiais'),('Petróleo'),('Produção'),('Eletrônica'),('Mecânica'),('Metalúrgica'),('Naval'),('Nuclear'),('Outros')") 
+    connection.commit()
